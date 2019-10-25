c46d16ecaaa22afa299d610d3b99aeea4f565c34
Nepcam <cameron.nepe@gmail.com>
2019-10-25 14:40:05 +1300

    added email and phone expressions

--- app.js
+++ app.js
@@ -6,23 +6,23 @@ document.getElementById('phone').addEventListener('blur', validatePhone);
 
 function validateName() {
   const name = document.getElementById('name');
-  const re = /^[a-zA-Z]{2, 10}$/;
+  const re = /^[a-zA-Z]{2,10}$/;
 
-  if(!re.test(name.vale)){                    // Using the test() method to see what the condition is
-    name.classList.add('is-invalid');         // Then using the add() add depending on the condtion or else 
+  if(!re.test(name.value)){                              // Using test() method to see what the condition is
+    name.classList.add('is-invalid');                    // add() method this sets off the 'is-invalid' class
   } else {
-    name.classList.remove('is-invalid');      // Using the remove() method to see what the condition is and change
+    name.classList.remove('is-invalid');                  // remove() method removes the 'is-invalid' class
   }
 }
 
 function validatePost() {
-  const post = document.getElementById('post');
-  const re = /^[0-9]{4}?$/;
+  const post = document.getElementById('zip');
+  const re = /^[0-9]{5}(-[0-9]{4})?$/;
 
-  if(!re.test(post.vale)){                    // Using the test() method to see what the condition is
-    post.classList.add('is-invalid');         // Then using the add() add depending on the condtion or else 
+  if(!re.test(post.value)){                         
+    post.classList.add('is-invalid');              
   } else {
-    post.classList.remove('is-invalid');      // Using the remove() method to see what the condition is and change
+    post.classList.remove('is-invalid');            
   }
 }
 
@@ -30,13 +30,20 @@ function validateEmail() {
   const email = document.getElementById('email');
   const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 
-  if(!re.test(email.vale)){                    // Using the test() method to see what the condition is
-    email.classList.add('is-invalid');         // Then using the add() add depending on the condtion or else 
+  if(!re.test(email.value)){
+    email.classList.add('is-invalid');
   } else {
-    email.classList.remove('is-invalid');      // Using the remove() method to see what the condition is and change
+    email.classList.remove('is-invalid');
   }
 }
 
 function validatePhone() {
-  
+  const phone = document.getElementById('phone');
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+  if(!re.test(phone.value)){
+    phone.classList.add('is-invalid');
+  } else {
+    phone.classList.remove('is-invalid');
+  }
 }
